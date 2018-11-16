9d0751e123bed82bad2e17e279d5ffdc09091bc4
Luyu Liu <liuluyu0378@gmail.com>
2018-11-16 11:46:14 -0500

    makeFeasible

--- cuckoo.js
+++ cuckoo.js
@@ -26,12 +26,12 @@
     Cuckoo.prototype = {
         simpleBounds: function (s, Lb, Ub){
             for (var i=0; i< s.length; i++){
-                if (s[i]<Lb[i]){
-                    s[i]=Lb[i]
+                if (s[i]<Lb){
+                    s[i]=Lb
                 }
                 else{
-                    if (s[i]>Ub[i]){
-                        s[i]=Ub[i]
+                    if (s[i]>Ub){
+                        s[i]=Ub
                     }
                 }
             }
@@ -39,31 +39,71 @@
         },
 
         makeFeasible: function (s, Lb, Ub){// need to fix 
-            all = new Set(Array.from(new Array(Ub-Lb+1), (x,i) => i + lb))
-            s1 = set(s)
-            if (s1 == s)
-            {                
+            all = Array.from(new Array(Ub-Lb+1), (x,i) => i + Lb);
+            s1 = _.uniq(s);
+            if (s1 == s){
                 return s
             }
-            diff = list(all.difference(s1))
-            return list(s1) + random.sample(diff, len(s)-len(s1))
+            diff = _.difference(all,s1);
+            return s1.concat(_.sample(diff, s.length - s1.length));
+        },
 
+        getBestNest: function(nests, nests_new, fitness){
+            n = len(nests)
+            fbest = fitness[0]
+            k = 0
+            for (var i in range(n)){
+
+                fnew = fobj(nests_new[i])
+                if fnew < fitness[i]:
+                    fitness[i] = fnew
+                    nests[i] = nests_new[i]
+                if fitness[i] < fbest:
+                    fbest = fitness[i]
+                    k = i
+
+            }
+            return [nests[k], k];
+        },
+
+        getCuckoos: function(nests, best){
+            n = len(nests)
+            d = len(best)
+            new_nests = []
+            for i in range(n):
+                s = nests[i]
+                u = [gauss(0, 1) * sigma for _ in range(d)]
+                v = [gauss(0, 1) for _ in range(d)]
+                step = [ u[j] / abs(v[j]) ** (1/beta) for j in range(d)]
+                stepsize = [0.01 * step[j] * (s[j]-best[j]) for j in range(d)]
+                s1 = [int(s[j]+stepsize[j]*gauss(0, 1)) for j in range(d)]
+                simplebounds(s1, lb, ub)
+                s1 = make_feasible(s1, lb, ub)
+                new_nests.append(s1)
+            return new_nests
+        },
+
+        emptyNests: function(nests, lb, ub, pa){
+            n = len(nests)
+            //Discovered or not -- a status vector
+            //determine which nest get to discover
+            k = [ 1 if random.random() > pa else 0 for _ in range(n)]
+            sample1 = random.sample(range(n), n)
+            sample2 = random.sample(range(n), n)
+            //those in sample1 difference sample2, then apply stepsize
+            new_nests = []
+            for i in range(n):
+                s1 = nests[sample1[i]]
+                s2 = nests[sample2[i]]
+                stepsize = [random.random() * (s1[j]-s2[j]) for j in range(len(s1)) ]
+                s = [int(nests[i][j] + stepsize[j] * k[j]) for j in range(len(s1))]
+                simplebounds(s, lb, ub)
+                s = make_feasible(s, lb, ub)
+                new_nests.append(s)
+            return new_nests
         }
 
 
-
-
-
-
-
     }
-
-    
-
-    function 
-
-
-
-
 
 })()
