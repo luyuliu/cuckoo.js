0bee7299c0a2e7f6a2269f40bb06876ae91ba07c
Luyu Liu <liuluyu0378@gmail.com>
2018-11-16 12:47:05 -0500

    More

--- cuckoo.js
+++ cuckoo.js
@@ -1,11 +1,11 @@
-(function(){
+(function () {
     'use strict';
 
-    function objectiveFunction(x){
+    function objectiveFunction(x) {
         return Math.sin(x)
     }
 
-    function Cuckoo(nestCount, dimension, maxGeneration, Pa, Lb, Ub ){
+    function Cuckoo(nestCount, dimension, maxGeneration, Pa, Lb, Ub) {
         // nestCount: The number of nests;
         // dimension: The dimension of x;
         // maxGeneration: Maximum generation of enumeration;
@@ -13,93 +13,96 @@
         // lb: Lower bound;
         // ub: Upper bound;
 
-        this.beta = 3/2; // recommended parameter
+        this.beta = 3 / 2; // recommended parameter
         this.sigma = 0.6965745025576967; // recommended parameter
 
         this.nests = []
 
-        
+
 
 
     }
 
     Cuckoo.prototype = {
-        simpleBounds: function (s, Lb, Ub){
-            for (var i=0; i< s.length; i++){
-                if (s[i]<Lb){
-                    s[i]=Lb
+        simpleBounds: function (s, Lb, Ub) {
+            for (var i = 0; i < s.length; i++) {
+                if (s[i] < Lb) {
+                    s[i] = Lb
                 }
-                else{
-                    if (s[i]>Ub){
-                        s[i]=Ub
+                else {
+                    if (s[i] > Ub) {
+                        s[i] = Ub
                     }
                 }
             }
             return s;
         },
 
-        makeFeasible: function (s, Lb, Ub){// need to fix 
-            all = Array.from(new Array(Ub-Lb+1), (x,i) => i + Lb);
+        makeFeasible: function (s, Lb, Ub) {// need to fix 
+            all = Array.from(new Array(Ub - Lb + 1), (x, i) => i + Lb);
             s1 = _.uniq(s);
-            if (s1 == s){
+            if (s1 == s) {
                 return s
             }
-            diff = _.difference(all,s1);
+            diff = _.difference(all, s1);
             return s1.concat(_.sample(diff, s.length - s1.length));
         },
 
-        getBestNest: function(nests, nests_new, fitness){
-            n = len(nests)
-            fbest = fitness[0]
-            k = 0
-            for (var i in range(n)){
-
-                fnew = fobj(nests_new[i])
-                if fnew < fitness[i]:
-                    fitness[i] = fnew
-                    nests[i] = nests_new[i]
-                if fitness[i] < fbest:
-                    fbest = fitness[i]
-                    k = i
-
+        getBestNest: function (nests, nests_new, fitness) {
+            var n = nests.length;
+            var fbest = fitness[0];
+            var k = 0;
+            var fnew;
+            for (var i in range(n)) {
+                fnew = objectiveFunction(nests_new[i])
+                if (fnew < fitness[i]) {
+                    fitness[i] = fnew;
+                    nests[i] = nests_new[i];
+                }
+                if (fitness[i] < fbest) {
+                    fbest = fitness[i];
+                    k = i;
+                }
             }
             return [nests[k], k];
         },
 
-        getCuckoos: function(nests, best){
-            n = len(nests)
-            d = len(best)
-            new_nests = []
-            for i in range(n):
-                s = nests[i]
+        getCuckoos: function (nests, best) {
+            n = len(nests);
+            d = len(best);
+            new_nests = [];
+            for (var i = 0; i < n; i++) {
+                s = nests[i];
                 u = [gauss(0, 1) * sigma for _ in range(d)]
                 v = [gauss(0, 1) for _ in range(d)]
-                step = [ u[j] / abs(v[j]) ** (1/beta) for j in range(d)]
-                stepsize = [0.01 * step[j] * (s[j]-best[j]) for j in range(d)]
-                s1 = [int(s[j]+stepsize[j]*gauss(0, 1)) for j in range(d)]
+                step = [u[j] / abs(v[j]) ** (1 / beta) for j in range(d)]
+                stepsize = [0.01 * step[j] * (s[j] - best[j]) for j in range(d)]
+                s1 = [int(s[j] + stepsize[j] * gauss(0, 1)) for j in range(d)]
                 simplebounds(s1, lb, ub)
                 s1 = make_feasible(s1, lb, ub)
                 new_nests.append(s1)
+            }
+
             return new_nests
         },
 
-        emptyNests: function(nests, lb, ub, pa){
+        emptyNests: function (nests, lb, ub, pa) {
             n = len(nests)
             //Discovered or not -- a status vector
             //determine which nest get to discover
-            k = [ 1 if random.random() > pa else 0 for _ in range(n)]
+            k = [1 if random.random() > pa else 0 for _ in range(n)]
             sample1 = random.sample(range(n), n)
             sample2 = random.sample(range(n), n)
             //those in sample1 difference sample2, then apply stepsize
             new_nests = []
             for i in range(n):
                 s1 = nests[sample1[i]]
-                s2 = nests[sample2[i]]
-                stepsize = [random.random() * (s1[j]-s2[j]) for j in range(len(s1)) ]
-                s = [int(nests[i][j] + stepsize[j] * k[j]) for j in range(len(s1))]
-                simplebounds(s, lb, ub)
-                s = make_feasible(s, lb, ub)
-                new_nests.append(s)
+            s2 = nests[sample2[i]]
+            stepsize = [random.random() * (s1[j] - s2[j]) for j in range(len(s1)) ]
+            s = [int(nests[i][j] + stepsize[j] * k[j]) for j in range(len(s1))]
+            simplebounds(s, lb, ub)
+            s = make_feasible(s, lb, ub)
+            new_nests.append(s)
             return new_nests
         }
 
