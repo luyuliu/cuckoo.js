ca4aea3ff45898c2d10ff532a5f1bd05dc30ecee
Luyu Liu <liuluyu0378@gmail.com>
2018-11-16 17:33:43 -0500

    No error

--- cuckoo.js
+++ cuckoo.js
@@ -1,5 +1,7 @@
+require("underscore");
+
 (function () {
-    'use strict';
+    //'use strict';
 
     function objectiveFunction(x) {
         return Math.sin(x)
@@ -13,18 +15,71 @@
         // lb: Lower bound;
         // ub: Upper bound;
 
+        _ = require("underscore")
         this.beta = 3 / 2; // recommended parameter
         this.sigma = 0.6965745025576967; // recommended parameter
 
-        this.nests = []
+        //--------------------------need to fix to Lb and Ub--------------------------
+        var lb, ub, n, d, maxgen, pa, nests, fitness, best, k;
+        Lb = 0; // lower bound
+        Ub = 99;// upper bound
+        this.lb = Lb;
+        this.ub = Ub;
+
+        //----------------------------------------------------------------------------
+
+        n = 10;  // number of nests
+        d = 5;   // number of dimensions (number of facilities)
+        maxgen = 100;
 
 
+        pa = 0.25; // Discovery rate of alien eggs/solutions
+
+        nests = [];
+        fitness = [];
+
+        for (var ii = 0; ii < n; ii++) {
+            nests.push(_.sample(Array.from(new Array(Ub - Lb + 1), (x, i) => i + Lb), d));
+        }
+        for (var ii = 0; ii < nests.length; ii++) {
+            fitness.push(10000000);
+        }
+
+        var a = [1, 2, 3, 4]
+        var tempObject = this._getBestNest(nests, nests, fitness);
+        best = tempObject[0];
+        k = tempObject[1];
+
+        for (var i=0; i < maxgen; i++) {
+            newNests = this._getCuckoos(nests, best)
+            var tempObject = this._getBestNest(nests, newNests, fitness)
+            best = tempObject[0];
+            k = tempObject[1];
+            newNests = this._emptyNests(nests, lb, ub, pa)
+            var tempObject = this._getBestNest(nests, newNests, fitness)
+            best = tempObject[0];
+            k = tempObject[1];
+            console.log("loop: ",best, fitness)
+        }
+
+        console.log(best, fitness[k])
 
 
     }
 
     Cuckoo.prototype = {
-        simpleBounds: function (s, Lb, Ub) {
+
+        init: function () {
+
+        },
+        _randNorm: function () {
+            var u = 0, v = 0;
+            while (u === 0) u = Math.random(); //Converting [0,1) to (0,1)
+            while (v === 0) v = Math.random();
+            return Math.sqrt(-2.0 * Math.log(u)) * Math.cos(2.0 * Math.PI * v);
+        },
+
+        _simpleBounds: function (s, Lb, Ub) {
             for (var i = 0; i < s.length; i++) {
                 if (s[i] < Lb) {
                     s[i] = Lb
@@ -38,8 +93,9 @@
             return s;
         },
 
-        makeFeasible: function (s, Lb, Ub) {// need to fix 
-            all = Array.from(new Array(Ub - Lb + 1), (x, i) => i + Lb);
+        _makeFeasible: function (s, Lb, Ub) {
+            var all, s1;
+            all = Array.from(new Array(this.ub - this.lb + 1), (x, i) => i + Lb);
             s1 = _.uniq(s);
             if (s1 == s) {
                 return s
@@ -48,12 +104,15 @@
             return s1.concat(_.sample(diff, s.length - s1.length));
         },
 
-        getBestNest: function (nests, nests_new, fitness) {
+        _getBestNest: function (nests, nests_new, fitness) {
             var n = nests.length;
             var fbest = fitness[0];
             var k = 0;
             var fnew;
-            for (var i in range(n)) {
+            
+            console.log(nests_new)
+            for (var i = 0; i < n; i++) {
+                // ____________________
                 fnew = objectiveFunction(nests_new[i])
                 if (fnew < fitness[i]) {
                     fitness[i] = fnew;
@@ -67,46 +126,79 @@
             return [nests[k], k];
         },
 
-        getCuckoos: function (nests, best) {
-            n = len(nests);
-            d = len(best);
-            new_nests = [];
+        _getCuckoos: function (nests, best) {
+            var n, d, u, v, step, stepSize, s1, newNests,s;
+            n = nests.length;
+            d = best.length;
+            newNests = [];
             for (var i = 0; i < n; i++) {
                 s = nests[i];
-                u = [gauss(0, 1) * sigma for _ in range(d)]
-                v = [gauss(0, 1) for _ in range(d)]
-                step = [u[j] / abs(v[j]) ** (1 / beta) for j in range(d)]
-                stepsize = [0.01 * step[j] * (s[j] - best[j]) for j in range(d)]
-                s1 = [int(s[j] + stepsize[j] * gauss(0, 1)) for j in range(d)]
-                simplebounds(s1, lb, ub)
-                s1 = make_feasible(s1, lb, ub)
-                new_nests.append(s1)
+                u = [];
+                v = [];
+                step = [];
+                stepSize = [];
+                s1=[];
+                for (var j = 0; j < d; j++) {
+                    u.push(this._randNorm() * this.sigma);
+                    v.push(this._randNorm());
+                    step.push(u[j] / Math.pow(Math.abs(v[j]) , (1 / this.beta)));
+                    stepSize.push(0.01 * step[j] * (s[j] - best[j]));
+                    s1.push(parseInt(s[j] + stepSize[j] *this. _randNorm()));
+                }
+                this._simpleBounds(s1, this.Lb, this.Ub);
+                s1 = this._makeFeasible(s1, this.Lb, this.Ub);
+                newNests.push(s1);
             }
 
-            return new_nests
+            return newNests;
         },
 
-        emptyNests: function (nests, lb, ub, pa) {
-            n = len(nests)
+        _emptyNests: function (nests, lb, ub, pa) {
+            var n, k, sample1, sample2, s1, s2, stepSize, s, newNests, tag;
+            n = nests.length;
+            k = [];
             //Discovered or not -- a status vector
             //determine which nest get to discover
-            k = [1 if random.random() > pa else 0 for _ in range(n)]
-            sample1 = random.sample(range(n), n)
-            sample2 = random.sample(range(n), n)
+            for (var i = 0; i < n; i++) {
+                tag = Math.random();
+                if (tag > pa) {
+                    k.push(1);
+                }
+                else {
+                    k.push(0);
+                }
+
+            }
+
+            sampleAll=[];
+            for (var a=0;a<n;a++){
+                sampleAll.push(a)
+            }
+            sample1 = _.sample(sampleAll, n);
+            sample2 = _.sample(sampleAll, n);
             //those in sample1 difference sample2, then apply stepsize
-            new_nests = []
-            for i in range(n):
-                s1 = nests[sample1[i]]
-            s2 = nests[sample2[i]]
-            stepsize = [random.random() * (s1[j] - s2[j]) for j in range(len(s1)) ]
-            s = [int(nests[i][j] + stepsize[j] * k[j]) for j in range(len(s1))]
-            simplebounds(s, lb, ub)
-            s = make_feasible(s, lb, ub)
-            new_nests.append(s)
-            return new_nests
+            newNests = []
+
+            for (var i = 0; i < n; i++) {
+                s1 = nests[sample1[i]];
+                s2 = nests[sample2[i]];
+                stepsize = [];
+                s=[];
+                for (var j = 0; j < s1.length; j++) {
+                    stepsize.push(Math.random() * (s1[j] - s2[j]));
+                    s.push(parseInt(nests[i][j] + stepsize[j] * k[j]));
+                }
+                this._simpleBounds(s1, this.Lb, this.Ub);
+                s = this._makeFeasible(s, this.Lb, this.Ub)
+                newNests.push(s)
+            }
+            return newNests;
         }
 
 
     }
 
+    var cuckoo = new Cuckoo();
+
 })()
+
